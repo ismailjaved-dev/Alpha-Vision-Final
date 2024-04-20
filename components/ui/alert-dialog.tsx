edaf2d38dedd8405a5ebc5613df0fb83675933a4
Ismailjaved_dev <Ismailjaved_dev>
2024-04-20 22:11:47 +0500

    video change and error resolve

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -116,10 +116,7 @@ const AlertDialogCancel = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Cancel
     ref={ref}
-    className={cn(
-      buttonVariants({ variant: "outline" }),
-      "mt-2 sm:mt-0",
-      className
+    className={cn("mt-2 sm:mt-0",className
     )}
     {...props}
   />
