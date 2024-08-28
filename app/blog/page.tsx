edde62399d4d57cb41362fdc9fcc1c1fed289bfb
JZtechDev <dev@jztech.co>
2024-08-28 20:42:51 +0500

    blog update

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,16 @@
-// import Blog from '@/components/sections/blog'
-// import Footer from '@/components/sections/footer'
-// import Hero from '@/components/sections/hero'
-// import React from 'react'
+import Blog from '@/components/sections/blog'
+import Footer from '@/components/sections/footer'
+import Hero from '@/components/sections/hero'
+import React from 'react'
 
-// const page = () => {
-//   return (
-//     <div>
-//       <Hero heading="Blog" desc="Home / Blog"/>
-//       <Blog/>
-//       <Footer />
-//     </div>
-//   )
-// }
+const page = () => {
+  return (
+    <div>
+      {/* <Hero heading="Blog" desc="Home / Blog"/>
+      <Blog/>
+      <Footer /> */}
+    </div>
+  )
+}
 
-// export default page
+export default page
