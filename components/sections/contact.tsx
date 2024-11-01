237a75291390e797c7c7633aa4d95d229c78b2c4
ismail javed <ismailjaved.dev@gmail.com>
2024-11-02 01:47:10 +0500

    address

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -37,8 +37,7 @@ const cardsData = [
     Icon: '/locationIcon.png',
     content: <>
       <p>25-08, Soho Capital,</p>
-      <p>Podomoro City,</p>
-      <p>Jakarta Barat,</p>
+      <p>Podomoro City, Jakarta Barat,</p>
       <p>DKI Jakarta 11470, Indonesia.</p>
     </>
   }]
